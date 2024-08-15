f7d93f8437f9e122dd1807b41e2c95f5a8496c72
Dinesh <dineshkarthie@gmail.com>
2024-08-15 14:42:55 +0530

    removed unwanted files

---
+++ vaccine/src/commonMain/kotlin/com/dineshworkspace/vaccine/AddVaccineComposable.kt
@@ -0,0 +1,1 @@
+package com.dineshworkspace.vaccine
--- vaccine/src/commonMain/kotlin/com/dineshworkspace/vaccine/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.dineshworkspace.vaccine
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
