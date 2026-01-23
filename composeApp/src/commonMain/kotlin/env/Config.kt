992fb3b46b15f88c294f8e107356c5a0f07c1861
Dinesh <dineshkarthie@gmail.com>
2026-01-23 18:30:25 +0530

    Added delete usecase and permission fix

--- composeApp/src/commonMain/kotlin/env/Config.kt
+++ composeApp/src/commonMain/kotlin/env/Config.kt
@@ -3,5 +3,5 @@ package env
 internal object Config {
     const val BASE_ENV = "Data"
     const val PETS_COLLECTION = "Pets"
-    const val VACCINE_NOTES_COLLECTION = "Vaccine Notes"
+    const val VACCINE_NOTES_COLLECTION = "Vaccines"
 }
