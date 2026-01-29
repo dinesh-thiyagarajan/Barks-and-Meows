821c7e09e5b3bb348e497b0a745d49068296419b
Dinesh <dineshkarthie@gmail.com>
2026-01-29 20:32:35 +0530

    Removed unwanted code

--- reminder/src/androidMain/kotlin/com/app/reminder/di/ReminderAndroidModule.kt
+++ reminder/src/androidMain/kotlin/com/app/reminder/di/ReminderAndroidModule.kt
@@ -1,12 +1,10 @@
 package com.app.reminder.di
 
-import com.app.reminder.dataSource.ReminderLocalDataSource
 import com.app.reminder.scheduler.ReminderSchedulerProvider
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val reminderAndroidModule = module {
-    single { ReminderLocalDataSource(androidContext()) }
     single { ReminderSchedulerProvider(androidContext()) }
     single<() -> Unit> {
         val schedulerProvider: ReminderSchedulerProvider = get()
