c50651be7e3589843b62305cc41b47cc1244c006
Dinesh <dineshkarthie@gmail.com>
2026-01-31 07:26:07 +0530

    Version Bump

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -68,8 +68,8 @@ android {
         applicationId = "com.app.barksandmeows"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
     }
     packaging {
         resources {
